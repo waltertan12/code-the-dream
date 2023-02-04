1d05c97d6eaf7739504dee39dc0b05b58c62862d
Walter Tan <wtan@brex.com>
2023-02-04 14:38:38 -0500

    Make pokemon select form dynamic

--- pokefusion/src/PokemonSelectForm.jsx
+++ pokefusion/src/PokemonSelectForm.jsx
@@ -1,18 +1,28 @@
 import pokemonList from './data/pokemonList.json';
+import { useState } from 'react';
 
 const PokemonSelectForm = () => {
+  const [pokemonId, setPokemonId] = useState(1);
+  const handlePokemonSelect = (event) => {
+    setPokemonId(Number(event.target.value));
+  };
+
   return (
     <div className="pokemon-select-form">
       <img
-        src="https://images.alexonsager.net/pokemon/1.png"
+        src={`https://images.alexonsager.net/pokemon/${pokemonId}.png`}
         alt="Blastoise"
         width="80"
         height="80"
       />
       <div className="pokemon-select-input">
-        <select>
+        <select onChange={handlePokemonSelect}>
           {pokemonList.map((pokemon) => {
-            return <option value={pokemon.id}>{pokemon.name}</option>;
+            return (
+              <option key={pokemon.id} value={pokemon.id}>
+                {pokemon.name}
+              </option>
+            );
           })}
         </select>
         <button>🔀</button>
