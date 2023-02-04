c6ea18852830a4cf1f00c7a1633cfd6ea2fc3ed4
Walter Tan <wtan@brex.com>
2023-02-04 12:58:42 -0500

    Initial commit

---
+++ pokefusion/src/util.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * @param {String} pokemonId
+ * @returns The image url of the pokemon
+ */
+export const getPokemonImageUrl = (pokemonId) =>
+  `https://images.alexonsager.net/pokemon/${pokemonId}.png`;
+
+/**
+ *
+ * @param {String} headId The pokemon id used for the head of the fused pokemon
+ * @param {String} bodyId The pokemon id used for the body of the fused pokemon
+ * @returns {string} The image url of the fused pokemon
+ */
+export const getPokefusionImageUrl = (headId, bodyId) =>
+  `https://images.alexonsager.net/pokemon/fused/${bodyId}/${bodyId}.${headId}.png`;
