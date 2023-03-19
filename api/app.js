585e50eba52c8fc635b756b434248b98bbadd88a
Walter Tan <wtan@brex.com>
2023-03-19 10:38:30 -0400

    Stub create user input

--- api/app.js
+++ api/app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
-const port = 3001;
+const port = process.env.SERVER_PORT || 3001;
 
 app.use(morgan("combined"));
 app.use(cors());
@@ -22,7 +22,7 @@ const generateProducts = (count = PRODUCT_COUNT) => {
 };
 const STATIC_PRODUCTS = generateProducts();
 
-app.get("/products", (req, res) => {
+app.get("/api/products", (req, res) => {
   let limit = LIMIT;
   if (req.query.limit) {
     limit = Number(req.query.limit);
@@ -40,12 +40,33 @@ app.get("/products", (req, res) => {
     cursor = Number(req.query.cursor);
   }
 
-  return res.json({
+  return res.status(200).json({
     data: STATIC_PRODUCTS.slice(cursor, cursor + limit),
     count: STATIC_PRODUCTS.length,
   });
 });
 
+app.post("/api/users", (request, response) => {
+  const body = request.body || {};
+  const delay = Math.floor(Math.random() * 500);
+  console.log({
+    message: "Received create user request",
+    body: request.body,
+    delay,
+  });
+
+  const now = new Date();
+  setTimeout(() => {
+    response.status(201).json({
+      id: uuid.v4(),
+      name: body.name || faker.internet.userName(),
+      email: body.email || faker.internet.email(),
+      insertedAt: now.toISOString(),
+      updatedAt: now.toISOString(),
+    });
+  }, delay);
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Starting server on port ${port}`);
 });
